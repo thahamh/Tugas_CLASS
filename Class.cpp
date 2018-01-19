6cda65633103b77f5f904d14fac81a13b2a82363
thahamh <32389972+thahamh@users.noreply.github.com>
2018-01-19 10:20:50 -0800

    Class tentang Mobil

---
+++ Class.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+class mobil{
+		public:
+				int jumlahMobil;
+				string merkMobil;
+				void berkendara(string kecepatan);
+				
+				
+}; 
+	
+int main (){
+	
+}
