7e2adf20109a21d8845d8645d771841801af2ae1
thahamh <32389972+thahamh@users.noreply.github.com>
2018-01-19 10:24:51 -0800

    Memberi nilai dari objek yang telah kita buat

--- Class.cpp
+++ Class.cpp
@@ -19,4 +19,21 @@ int main (){
 	
 	mobil mobilSedan, mobilCoupe, mobilSportCars, mobilCityCar, mobilMVP, mobilSUV;
 	
+	mobilSedan.jumlahMobil =50;
+	mobilSedan.merkMobil = "Honda Accord";
+	
+	mobilCoupe.jumlahMobil = 25;
+	mobilCoupe.merkMobil = "BMW series 6 coupe";
+	
+	mobilSportCars.jumlahMobil =10;
+	mobilSportCars.merkMobil = "Lamborghini";
+
+	mobilCityCar.jumlahMobil =60;
+	mobilCityCar.merkMobil = "Honda Jazz";
+	
+	mobilMVP.jumlahMobil =100;
+	mobilMVP.merkMobil = "Suzuki Ertiga";
+	
+	mobilSUV.jumlahMobil =40;
+	mobilSUV.merkMobil = "Toyota Rush";
 }
