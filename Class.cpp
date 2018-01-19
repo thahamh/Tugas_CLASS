8017204c277ec264efc8014525fe6dbc47e7c94a
thahamh <32389972+thahamh@users.noreply.github.com>
2018-01-19 10:28:40 -0800

    Menampilkan Jumlah Mobil, Jumlah Unit Mobil, Merk Mobil, dan Kecepatan Mobil

--- Class.cpp
+++ Class.cpp
@@ -36,4 +36,39 @@ int main (){
 	
 	mobilSUV.jumlahMobil =40;
 	mobilSUV.merkMobil = "Toyota Rush";
+	
+	string pilihan;
+	cout<< "Pilih Jenis Mobil Yang ingin Anda ketahui : "; cin >> pilihan;
+	
+	if(pilihan=="mobilSedan"){
+		cout << "Mobil Sedan :" <<endl;
+		cout << " - Jumlah Mobil = "<<mobilSedan.jumlahMobil<<"unit"<<endl;
+		cout << " - Merk Mobil   = "<<mobilSedan.merkMobil<<endl; 
+		mobilSedan.berkendara("125km/jam");
+	}else if(pilihan=="mobilCoupe"){
+		cout << "Mobil Coupe :"<<endl;
+		cout << " - Jumlah Mobil ="<<mobilCoupe.jumlahMobil<<"unit"<<endl;
+		cout << " - Merk Mobil   ="<<mobilCoupe.merkMobil<<endl;
+		mobilCoupe.berkendara("160km/jam");	
+	}else if(pilihan=="mobilSportcars"){
+		cout << "Mobil Sport Cars :"<<endl;
+		cout << " - Jumlah Mobil ="<<mobilSportCars.jumlahMobil<<"unit"<<endl;
+		cout << " - Merk Mobil   ="<<mobilSportCars.merkMobil<<endl;
+		mobilSportCars.berkendara("350km/jam");	 
+	}else if(pilihan=="mobilCitycar"){
+		cout << "Mobil CityCar :"<<endl;
+		cout << " - Jumlah Mobil ="<<mobilCityCar.jumlahMobil<<"unit"<<endl;
+		cout << " - Merk Mobil   ="<<mobilCityCar.merkMobil<<endl;
+		mobilCityCar.berkendara("130km/jam");	
+	}else if(pilihan=="mobilMVP"){
+		cout << "Mobil MVP (Multi Purpose Vehicle ) :"<<endl;
+		cout << " - Jumlah Mobil ="<<mobilMVP.jumlahMobil<<"unit"<<endl;
+		cout << " - Merk Mobil   ="<<mobilMVP.merkMobil<<endl;
+		mobilMVP.berkendara("135km/jam");	
+	}else if(pilihan=="mobilSUV"){
+		cout << "Mobil SUV (Sport Utility Vehicle ) :"<<endl;
+		cout << " - Jumlah Mobil ="<<mobilSUV.jumlahMobil<<"unit"<<endl;
+		cout << " - Merk Mobil   ="<<mobilSUV.merkMobil<<endl;
+		mobilSUV.berkendara("140km/jam");	
+	}
 }
